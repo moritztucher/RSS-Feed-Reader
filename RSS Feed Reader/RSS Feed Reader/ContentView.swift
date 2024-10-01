d8b7093d1d6f6de2bdd2eba8a62258f9bceccadd
Moritz Tucher <moritztucher@gmail.com>
2024-10-01 12:08:19 +0200

    Project Setup

---
+++ RSS Feed Reader/RSS Feed Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RSS Feed Reader
+//
+//  Created by Moritz Tucher on 01.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RSS Feed Reader/RSS Feed Reader/RSS_Feed_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_Feed_ReaderApp.swift
+//  RSS Feed Reader
+//
+//  Created by Moritz Tucher on 01.10.24.
+//
+
+import SwiftUI
+
+@main
+struct RSS_Feed_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
